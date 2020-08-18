69794467b7803a5bded9452c4e90dc2a60b65285
felord.cn <dax@felord.cn>
2020-08-18 13:24:39 +0800

    spring cache

--- kono-app/src/main/resources/sql/ddl.sql
+++ kono-app/src/main/resources/sql/ddl.sql
@@ -1,8 +1,17 @@
--- auto-generated definition
+create schema kono collate utf8mb4_unicode_ci;
+
 create table user_info
 (
-    user_id int auto_increment
+    user_id varchar(32) not null
         primary key,
-    name    varchar(16) null,
-    age     int         null
-);
+    username varchar(16) not null comment '用户名',
+    pass_word varchar(128) not null comment '密码',
+    enabled tinyint default 1 null comment '是否可用  0 否 1 是',
+    add_time datetime null,
+    update_time datetime null,
+    constraint user_info_username_uindex
+        unique (username)
+)
+    comment '用户表';
+
+
